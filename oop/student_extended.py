fade0c67a87d66ace967e235de2d7654b78975a9
irihojaphet <irihojaja@yahoo.com>
2026-03-07 18:35:17 +0200

    oop topic

---
+++ oop/student_extended.py
@@ -0,0 +1,20 @@
+class Student:
+    def __init__(self):
+        ...
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+def get_student():
+    
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house)
+    return student
+    
+
+
+if __name__ == "__main__":
+    main()
