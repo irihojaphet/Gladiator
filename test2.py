41a418230685b5ae607090df94d2b096f643dcd1
irihojaphet <irihojaja@yahoo.com>
2026-03-07 18:36:37 +0200

    oop topic

--- test2.py
+++ test2.py
@@ -1,19 +1,16 @@
 def main():
-    print(intersection([1,2,2,1],[2,2]))
-def intersection(nums1, nums2):
-    nums = []
-    test = []
+    print(isAnagram("", "tehpaj"))
 
-    for i in range(len(nums1)):
-        for j in range(len(nums2)):
-            if i != j:
-                if nums1[i] == nums2[j]:
-                    test.append(nums1[i])
-                    
-    for k in test:
-        if k not in nums:
-            nums.append(k)
-    return nums
+def isAnagram(s, t):
+    t_ = list(t)
+    for ch in list(s):
+        try:
+            t_.remove(ch)
+        except ValueError as e:
+            return False
+        
+    if t_:
+        return False
+    return True
 
 main()
-
