b1aa30d62d429dd2aea40469152c710620781a2c
irihojaphet <irihojaja@yahoo.com>
2026-03-08 23:18:21 +0200

    Getters and Setters

--- oop/student_extended.py
+++ oop/student_extended.py
@@ -1,23 +1,40 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
     def __str__(self):
         return f"{self.name} from {self.house}"
 
+
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "Stag charm!"
+            case "Otter":
+                return "Otter charm!"
+            case "Jack Russell terrier":
+                return "Jack Russell terrier charm!"
+            case _:
+                return "Unknown patronus charm."
+
+
+
 def main():
     student = get_student()
-    print(student)
-
+    print("Expecto Patronum!")
+    print(student.charm())
+    
 def get_student():
     
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    patronus = input("Partronus: ")
+    return Student(name, house, patronus)
     
 
 
